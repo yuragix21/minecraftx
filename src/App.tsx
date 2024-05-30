e4cd7e1d2da2c3c6bd81e55c1aa820c5344ee547
yuragix21 <torviscoadri20@gmail.com>
2024-05-29 22:58:16 -0500

    changues

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ function App() {
 
   return (
     <>
+    <header>
+      <li>hola</li>
+      <li>hola</li>
+      <li>hola</li>
+    </header>
      <section>
         <div>
           <h1>HOLA</h1>
         </div>
      </section>
+     <footer>joder</footer>
     </>
   )
 }
